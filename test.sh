ffe9965fa3254c64894c952a80c356d7a89ad174
Jordan Williams <jordan@jwillikers.com>
2021-06-19 13:55:57 -0500

    Remove comment

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 set -o errexit
 
 podman run --rm --name test-container -dt localhost/podman-systemd
-#podman run --volume --rm --name test-container -it localhost/podman-systemd
 sleep 5
 podman exec test-container podman --version
 podman exec test-container systemctl --version
