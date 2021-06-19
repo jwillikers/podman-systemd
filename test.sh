765102de6e1532cf07a3f9830eca5077ce3985d3
Jordan Williams <jordan@jwillikers.com>
2021-06-19 12:25:03 -0500

    Workaround Podman / Systemd bug in CI

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# podman run --cgroup-manager cgroupfs --systemd always --rm --name test-container -dt localhost/podman-systemd
-podman run --volume /sys/fs/cgroup:/sys/fs/cgroup:ro --cgroup-manager cgroupfs --systemd always --rm --name test-container -it localhost/podman-systemd
+podman run --systemd always --rm --name test-container -dt localhost/podman-systemd
+#podman run --volume --systemd always --rm --name test-container -it localhost/podman-systemd
 sleep 5
 podman exec test-container podman --version
 podman exec test-container systemctl --version
