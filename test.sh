29474c3a1ef15f958223c72775989ddaebede159
Jordan Williams <jordan@jwillikers.com>
2021-06-19 11:37:05 -0500

    Make interactive for CI

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-podman run --cgroup-manager cgroupfs --systemd always --rm --name test-container -dt localhost/podman-systemd
-# podman run --cgroup-manager cgroupfs --systemd always --rm --name test-container -it localhost/podman-systemd
+# podman run --cgroup-manager cgroupfs --systemd always --rm --name test-container -dt localhost/podman-systemd
+podman run --cgroup-manager cgroupfs --systemd always --rm --name test-container -it localhost/podman-systemd
 sleep 5
 podman exec test-container podman --version
 podman exec test-container systemctl --version
