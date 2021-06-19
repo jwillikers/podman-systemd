92c13d757905cc427d997bda77900e0a6aaea9f5
Jordan Williams <jordan@jwillikers.com>
2021-06-19 10:10:17 -0500

    Add debug output for CI

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-podman run --systemd always --rm --name test-container -dt localhost/podman-systemd
+# podman run --systemd always --rm --name test-container -dt localhost/podman-systemd
+podman run --systemd always --rm --name test-container -it localhost/podman-systemd
 sleep 5
 podman exec test-container podman --version
 podman exec test-container systemctl --version
